485f6ee37ec30746f0aa6b4716ab2616ebffb598
yufan0107 <109130552+yufan0107@users.noreply.github.com>
2022-07-13 12:01:06 +0800

    V1

---
+++ 0713/script.js
@@ -0,0 +1,6 @@
+// function change() {
+//     document.body.style.background = document.getElementById("colors").value
+// }
+document.getElementById("colors").addEventListener("change", function() {
+    document.body.style.background = document.getElementById("colors").value
+})
